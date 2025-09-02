4212b8e193d63b45bc8a25aa4d500cbd096c9579
Akash Koche <koche.akash@gmail.com>
2025-09-02 10:04:50 +0530

    Error Correction

--- src/main.py
+++ src/main.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flash(__name__)
 
-@app.router('/')
-def hello_world();
+@app.route('/')
+def hello_world():
     return 'Hello, World!'
 
-if __name__ -- '__main__';
+if __name__ -- '__main__':
     app.run(host='0.0.0.0', port=5000)
